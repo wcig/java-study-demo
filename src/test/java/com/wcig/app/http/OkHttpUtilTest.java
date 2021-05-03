dbf3ffff59e6fdc3b9f053ae1021dc359d85eec3
wcig <job.yangbo@gmail.com>
2021-05-03 12:49:50 +0800

    okhttp3

--- src/test/java/com/wcig/app/http/OkHttpUtilTest.java
+++ src/test/java/com/wcig/app/http/OkHttpUtilTest.java
@@ -3,44 +3,76 @@ package com.wcig.app.http;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
-import org.springframework.http.HttpStatus;
 
+import java.io.File;
 import java.io.IOException;
 
+/**
+ * 测试下来okhttp3不大好用..
+ */
 @Slf4j
 public class OkHttpUtilTest {
 
     @Test
-    public void test() throws IOException, InterruptedException {
-        log.info("okhttp");
-        String url = "http://localhost:28081/get";
+    public void testGet() throws IOException {
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url).build();
+        Request request = new Request.Builder()
+                .url("http://localhost:28080/get?id=100&name=tom")
+                .method("GET", null)
+                .build();
         Response response = client.newCall(request).execute();
-        if (response.code() != HttpStatus.OK.value()) {
-            log.error("response code not 200");
-            return;
-        }
-
-        // 同步
         Result result = JSON.parseObject(response.body().string(), Result.class);
-        log.info("response result: {}", result);
+        log.info("result: {}", result);
+    }
 
-        // 异步
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                log.error("response code not 200: {}", e.getMessage());
-            }
+    @Test
+    public void testPostJson() throws IOException {
+        User user = new User().setId(100).setName("tom");
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(JSON.toJSONString(user), mediaType);
+        Request request = new Request.Builder()
+                .url("http://localhost:28080/postJson")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        Result result = JSON.parseObject(response.body().string(), Result.class);
+        log.info("result: {}", result);
+    }
 
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                log.info("response: {}", response);
-            }
-        });
+    @Test
+    public void testPostFormUrlencoded() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create("id=100&name=tom", mediaType);
+        Request request = new Request.Builder()
+                .url("http://localhost:28080/postFormUrlencoded")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Response response = client.newCall(request).execute();
+        Result result = JSON.parseObject(response.body().string(), Result.class);
+        log.info("result: {}", result);
+    }
 
-        Thread.sleep(1000);
+    @Test
+    public void testPostFormData() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("id", "100")
+                .addFormDataPart("name", "tom")
+                .addFormDataPart("file", "test.png",
+                        RequestBody.create(new File("/Users/yangbo/Pictures/test.png"),
+                                MediaType.parse("application/octet-stream")))
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:28080/postFormData")
+                .method("POST", body)
+                .build();
+        Response response = client.newCall(request).execute();
+        Result result = JSON.parseObject(response.body().string(), Result.class);
+        log.info("result: {}", result);
     }
 }
